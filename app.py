3bb13ee264c013016bb45b8f897a529880493ed6
[low-ise] <[louise.caitano@proton.me]>
2024-03-21 12:00:52 -0300

    Cria as pastas templates e static

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('Hello')
 
 @app.route('/')
-
+@app.route('/teste')
 def hello():
     return "Hello world, buenos días, muchachosssss"
+
+@app.route('/cat')
+def alunos():
+    return render_template('hello.html')
