0f0a3e41d96726bebc3b3a8958d31bb57def23a4
[low-ise] <[louise.caitano@proton.me]>
2024-03-27 10:55:54 -0300

    Cria o banco de dados e estabelece sua conexão

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask('Hello')
 
-@app.route('/')
-@app.route('/teste')
-def hello():
-    return "Hello world, buenos días, muchachosssss"
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
+app.configure.from_object(__name__)
 
-@app.route('/cat')
-def alunos():
+@app.route('/')
+
+def conect():
+    return sqlite3.connect(DATABASE)
+
+def before_request():
+    g.bd = connect()
+
+def teardown_request():
+    g.bd.close()
+
+
+def hello():
     return render_template('hello.html')
